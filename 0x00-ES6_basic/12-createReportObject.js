d4ed801bc1cbc401a4197985dc1cc4d40e163759
Jukunye <jukunyes@gmail.com>
2024-04-08 16:59:53 +0300

    refactor: 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,7 +1,8 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments: (employeesList) =>
-      Object.keys(employeesList).length,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
   };
 }
