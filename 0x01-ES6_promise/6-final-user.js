52f0f1da801b01a81d453bf715356254fe1f094c
Jukunye <jukunyes@gmail.com>
2024-04-10 23:11:37 +0300

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const photo = uploadPhoto(fileName);
+  const user = signUpUser(firstName, lastName);
+
+  return Promise.allSettled([photo, user]).then((values) => {
+    const profileResults = values.map((value) => ({
+      status: value.status,
+      value: value.status === 'fulfilled' ? value.value : value.reason,
+    }));
+
+    return profileResults;
+  });
+}
